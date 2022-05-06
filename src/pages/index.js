4cd7d93fae57bcedbfb2f24af3a2cdc3d9e74f1e
Peter <peterthomaskim@gmail.com>
2022-05-05 22:45:27 -0300

    parte 3 completado

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
-const IndexPage=()=>{
-  return(
+const IndexPage = () => {
+  return (
     <Layout pageTitle="Home Page">
       <p>I`m making this by following the gatsby tutorial</p>
+      <StaticImage
+        alt="eps fillers do one piece"
+        src="../images/ex_one_piece_fillers.png"
+      ></StaticImage>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
