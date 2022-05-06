82d8979d25e7d67d39801b76190170c2f9d4511a
Peter <peterthomaskim@gmail.com>
2022-05-05 23:10:09 -0300

    my cool blogpost

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
   siteMetadata: {
+    title:"One piece eps fillers",
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve:"gatsby-source-filesystem",
+      options:{
+        name:`blog`,
+        path:`${__dirname}/blog`,
+      }
+    }
   ],
 }
